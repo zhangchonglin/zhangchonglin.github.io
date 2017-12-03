bc91d0898fbfbccc84cafb17af1fe0bb0e305132
zhangchonglin <zhangchonglin@users.noreply.github.com>
2017-12-03 17:23:31 -0500

    Update copyright.js

--- copyright.js
+++ copyright.js
@@ -7,10 +7,10 @@ document.write('<p>Copyright &copy ');
 var thisyear = new Date().getFullYear();
 if(thisyear > 2013){document.write('2013 - ');}
 document.write(thisyear+' Chonglin Zhang - All rights reserved.</p>');
-//document.write('        <p><!-- hitwebcounter Code START -->');
-//document.write('        <a href="http://www.hitwebcounter.com/countersiteservices.php" target="_blank">');
-//document.write('        <img src="http://hitwebcounter.com/counter/counter.php?page=4375313&style=0006&nbdigits=5&type=ip&initCount=0" title="Track Site Visits" Alt="Track Site %Visits"   border="0" >');
-//document.write('        </a>');
-//document.write('        </p>');
+document.write('        <p><!-- hitwebcounter Code START -->');
+document.write('        <a href="http://www.hitwebcounter.com/countersiteservices.php" target="_blank">');
+document.write('        <img src="http://hitwebcounter.com/counter/counter.php?page=4375313&style=0006&nbdigits=5&type=ip&initCount=0" title="Track Site Visits" Alt="Track Site %Visits"   border="0" >');
+document.write('        </a>');
+document.write('        </p>');
 document.write('<div style="text-align: center"><script type="text/javascript" src="http://jf.revolvermaps.com/2/4.js?i=5ep26w7jpz3&amp;m=0&amp;h=128&amp;c=ff0000&amp;r=0" async="async"></script></div>');
 document.write('    </div>');
